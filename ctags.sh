38540ddf12524ffecf1a0993fa5db22751238bc0
Mitko <mitkofr@yahoo.fr>
2010-01-20 14:44:10 +0000

    Added explanations and regex for functions

--- ctags.sh
+++ ctags.sh
@@ -3,4 +3,10 @@
 # (?:abstract|final) doesn't work although it is an extended POSIX regex
 # Two folders are taken into consideration in our example:
 
-ctags -R --regex-PHP='/\s*abstract\s+class\s+(\w+)/\1/' --regex-PHP='/\s*final\s+class\s+(\w+)/\1/' --regex-PHP='/\s*interface\s+(\w+)/\1/' . /usr/share/php/
+# tags generation for:
+#
+#     abstract|final     class     (class alone auto generated)
+#       public|protected function  (private unnecessary)
+#                        interface
+
+ctags -R --regex-PHP='/\s*abstract\s+class\s+(\w+)/\1/' --regex-PHP='/\s*final\s+class\s+(\w+)/\1/' --regex-PHP='/\s*public\s+function\s+(\w+)/\1/' --regex-PHP='/\s*protected\s+function\s+(\w+)/\1/' --regex-PHP='/\s*interface\s+(\w+)/\1/' . /usr/share/php/
