272f97abce0c3c4f1ad6da859812b6530790f569
mitko <mitko@winterfell.(none)>
2012-01-25 23:13:18 +0000

    Corrected path errors for fnotify file

--- .irssi/fnotify.bash
+++ .irssi/fnotify.bash
@@ -2,8 +2,9 @@
 
 # ssh remote.system.somewhere "tail -n 10 .irssi/fnotify ; : > .irssi/fnotify ; tail -f .irssi/fnotify " | sed -u 's/[<@&]//g' | while read heading message  do  notify-send -i gtk-dialog-info -t 300000 -- "${heading}" "${message}"; done # the sed -u 's/[<@&]//g' is needed as those characters might confuse  notify-send (FIXME: is that a bug or a feature?)
 
-> fnotify
-tailf fnotify | sed -u 's/[<@&]//g' |\
-while read heading message; do
-   notify-send -i gtk-dialog-info -t 300000 -- "${heading}" "${message}"
+> "$HOME"/.irssi/fnotify
+tailf "$HOME"/.irssi/fnotify | sed -u 's/[<@&]//g' |\
+while read heading message
+do
+   notify-send -i gtk-dialog-info -t 300000 -- "$heading" "$message"
 done
