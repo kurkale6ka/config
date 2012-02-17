c1fba0d9a2059d45cc9e3ff009b0deb60a45a061
mitko <mitkofr@yahoo.fr>
2012-02-17 17:12:27 +0000

    fnotify -> changed message limit to 5min theme   -> nothing... maybe permissions? config  -> alias /cc for clearing crap, /^msg: keep in current window...

--- .irssi/fnotify.bash
+++ .irssi/fnotify.bash
@@ -2,9 +2,10 @@
 
 # ssh remote.system.somewhere "tail -n 10 .irssi/fnotify ; : > .irssi/fnotify ; tail -f .irssi/fnotify " | sed -u 's/[<@&]//g' | while read heading message  do  notify-send -i gtk-dialog-info -t 300000 -- "${heading}" "${message}"; done # the sed -u 's/[<@&]//g' is needed as those characters might confuse  notify-send (FIXME: is that a bug or a feature?)
 
+# Keep notification window for 5 minutes = 300000 milliseconds
 > "$HOME"/.irssi/fnotify
 tailf "$HOME"/.irssi/fnotify | sed -u 's/[<@&]//g' |\
 while read heading message
 do
-   notify-send -i gtk-dialog-info -t 30000 -- "$heading" "$message"
+   notify-send -i gtk-dialog-info -t 300000 -- "$heading" "$message"
 done
