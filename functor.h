8c5553803eb915ec2a8ddefcd1b35dfb59ed6342
Dimitar Dimitrov <mitkofr@yahoo.fr>
2013-04-01 11:49:13 +0100

    Functions are functors

--- functor.h
+++ functor.h
@@ -10,3 +10,20 @@ So we would have ... (x -> y) -> Either a x -> Either a y
 To match Either type1 type2,         \            /
   we use Right x                     Left unchanged
      and Left x
+
+--------------------------------------------------------------------------------
+
+Functions are functors
+                     g
+instance Functor ((->) r) where
+    fmap f g = (\x -> f (g x))
+          g x         f (g x)
+OR
+    fmap = (.), so f . g is the same as fmap f g
+
+a -> b is a type, the type of a function =>
+(->) a is a type constructor expecting one parameter only
+
+Resemblances: [2,11,..] :: [Int]
+              Just 'a'  :: Maybe Char
+              g 2.3     :: r -> Float
