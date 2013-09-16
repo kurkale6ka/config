bc8aab3072687d833bd9676cba6d529e2ce9ee60
Dimitar Dimitrov <mitkofr@yahoo.fr>
2013-09-16 10:28:47 +0100

    No assignments here!

--- git_conf.bash
+++ git_conf.bash
@@ -4,7 +4,7 @@ git config --global user.name 'Dimitar Dimitrov'
 git config --global user.email mitkofr@yahoo.fr
 
 git config --global core.excludesfile "$HOME"/.gitignore
-git-config --global core.editor "vim -u $HOME/.vimrc"
+git config --global core.editor "vim -u $HOME/.vimrc"
 git config --global color.ui true
 
 # make 'git pull' on master always use rebase
@@ -16,14 +16,14 @@ git config --global diff.tool vimdiff
 git config --global difftool.prompt false
 
 git config --global alias.d difftool
-git config --global alias.df=diff
-git config --global alias.co=checkout
-git config --global alias.ci=commit
-git config --global alias.st=status
-git config --global alias.br=branch
-git config --global alias.hist=log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
-git config --global alias.type=cat-file -t
-git config --global alias.dump=cat-file -p
+git config --global alias.df diff
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.type cat-file -t
+git config --global alias.dump cat-file -p
 
 # # Http tunneling
 # # --------------
