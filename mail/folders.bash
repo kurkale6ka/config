a909416444eb36d1dacb68b880802369e8b0a28b
Dimitar Dimitrov <mitkofr@yahoo.fr>
2013-12-04 13:45:41 +0000

    Update

--- mail/folders.bash
+++ mail/folders.bash
@@ -8,9 +8,11 @@ accounts+=(work)
 accounts+=(yahoo)
 
 for a in "${accounts[@]}"; do
-   mkdir -p "$folder/$a"/{archive,drafts,inbox,sent}/{cur,new,tmp}
+   mkdir -p "$folder/$a"/{drafts,inbox,sent}/{cur,new,tmp}
+   mkdir -p "$folder/$a"/inbox/archive/{cur,new,tmp}
 done
 
 mkdir -p "$folder"/google/inbox/vim_{dev,use}/{cur,new,tmp}
+mkdir -p "$folder"/google/inbox/vim_{dev,use}/archive/{cur,new,tmp}
 
 # "$HOME"/github/config/mail/work.bash
