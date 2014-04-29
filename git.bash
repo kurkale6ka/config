26fabf52ec2dc68af4da4b1a94040d6393b324b1
Dimitar Dimitrov <mitkofr@yahoo.fr>
2014-04-29 15:53:34 +0100

    git log format: improve visually by aligning vertically and adding more color

--- git.bash
+++ git.bash
@@ -22,7 +22,7 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global alias.br branch
-git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.l "log --graph --date=short --pretty=format:'%C(yellow)%h%C(reset) %C(green)%ad%C(reset) %C(blue)%an%C(reset) | %s%d'"
 git config --global alias.type cat-file -t
 git config --global alias.dump cat-file -p
 
