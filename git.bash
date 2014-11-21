d6c37971f0191e049d7b9111ea597497db9a54bd
Dimitar Dimitrov <mitkofr@yahoo.fr>
2014-11-21 23:39:43 +0000

    git sha alias

--- git.bash
+++ git.bash
@@ -20,6 +20,7 @@ git config --global alias.d difftool
 git config --global alias.df diff
 git config --global alias.co checkout
 git config --global alias.ci commit
+git config --global alias.sha "rev-parse HEAD"
 git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.l "log --date=short --pretty=format:'%C(yellow)%h%C(reset) %C(green)%ad%C(reset) %C(blue)%an%C(reset) | %s %C(red)%d%C(reset)'"
