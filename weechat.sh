4de4fae36eab3a8201c0838bd07e53783da4ed11
Dimitar Dimitrov <mitkofr@yahoo.fr>
2018-10-06 21:03:58 +0100

    Transform into a script

--- weechat.sh
+++ weechat.sh
@@ -1,13 +1,36 @@
-# Paste this configuration into weechat using this command:
-# grep -v '^#\|^$' $REPOS_BASE/config/weechat | sed -n '1,/sasl_username/p' | xclip
+#! /usr/bin/env bash
+
+cb_file="$(mktemp)"
 
 # TLS
 # Mac OS
-/set weechat.network.gnutls_ca_file "/usr/local/etc/openssl/cert.pem"
+if [[ -r /usr/local/etc/openssl/cert.pem ]]
+then
+   echo '/set weechat.network.gnutls_ca_file "/usr/local/etc/openssl/cert.pem"' > "$cb_file"
 # Linux
-# /set weechat.network.gnutls_ca_file "/etc/ssl/certs/ca-certificates.crt"
+elif [[ -r /etc/ssl/certs/ca-certificates.crt ]]
+then
+   echo '/set weechat.network.gnutls_ca_file "/etc/ssl/certs/ca-certificates.crt"' > "$cb_file"
+else
+   echo 'No valid certificates found' 1>&2
+   rm "$cb_file"
+   exit 1
+fi
 
-# Servers: freenode, OFTC
+# OFTC certificate
+echo 'Generating certificate for authentication on OFTC...'
+mkdir -p ~/.weechat/certs
+if cd ~/.weechat/certs
+then
+   openssl req -nodes -newkey rsa:2048 -keyout nick.key -x509 -days 3650 -out nick.cert
+   openssl x509 -in nick.cert -noout -fingerprint
+   cat nick.cert nick.key > nick.pem
+   chmod 400 nick.*
+fi
+
+cat >> "$cb_file" << 'COPY'
+
+# Servers
 /server add freenode chat.freenode.net/7000 -ssl -autoconnect
 /server add OFTC irc.oftc.net/6697 -ssl -autoconnect
 
@@ -50,12 +73,12 @@
 /set weechat.look.bar_more_right "▶"
 /set weechat.look.bar_more_up "▲"
 
-# Plugins...
+# Plugins
 # /script install lnotify.py (requires notify-send)
 # /script install urlserver.py
 # /script install buffer_autoset.py histman.py
 
-# bars (...wait for plugins to finish installing)
+# Bars (...wait for plugins to finish installing)
 /set weechat.bar.buffers.size_max 11
 /set weechat.bar.input.size 0
 /set weechat.bar.title.color_bg 60
@@ -67,7 +90,7 @@
 /set aspell.check.real_time on
 /set aspell.check.default_dict en
 
-# nicks
+# Nicks
 /bar hide nicklist
 /alias add nicks /bar toggle nicklist
 /set weechat.color.chat_nick_colors "cyan,magenta,green,brown,lightblue,default,lightcyan,lightmagenta,lightgreen,blue,31,35,38,40,49,63,70,80,92,99,112,126,130,138,142,148,160,162,167,169,174,176,178,184,186,210,212,215,247"
@@ -75,17 +98,22 @@
 /key missing
 /mouse enable
 
-/save
-
-# sasl
 /set irc.server.freenode.sasl_username kurkale6ka
+COPY
+
+# Copy commands to clipboard
+grep -v '^#\|^$' "$cb_file" | xclip
+
+cat << 'MSG'
+
+Freenode
 /set irc.server.freenode.sasl_password **********
 
-# certificate authentication for OFTC
-mkdir ~/.weechat/certs && cd "$_"
-openssl req -nodes -newkey rsa:2048 -keyout nick.key -x509 -days 3650 -out nick.cert
-cat nick.cert nick.key > nick.pem
-chmod 400 nick.*
-/msg nickserv identify
-# openssl x509 -in nick.cert -noout -fingerprint
+OFTC
+/msg nickserv identify **********
 /msg nickserv cert add
+
+/save
+MSG
+
+rm "$cb_file"
