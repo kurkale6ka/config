05b80398060fe2c1e74624baea5733918b6ca6a8
Dimitar Dimitrov <mitkofr@yahoo.fr>
2020-07-07 21:20:45 +0100

    Process arguments and ranges

--- tmux/lay.pl
+++ tmux/lay.pl
@@ -18,16 +18,56 @@ my $R = color('reset');
 sub help() {
    print <<MSG;
 ${S}SYNOPSIS${R}
-${S}OPTIONS${R}
+lay host[n-z]
+${S}RANGES${R}
+  - : 1..2
+ -3 : 1..3
+3-6 : 3..6
 ${S}DESCRIPTION${R}
 MSG
-   exit;
+exit;
 }
 
-# Arguments
 GetOptions(
    'h|help' => \&help
 ) or die RED.'Error in command line arguments'.RESET, "\n";
+
+@ARGV == 0 and help;
+
+# Arguments
+my @hosts;
+
+foreach (@ARGV)
+{
+   # get host and range
+   my ($host, $first, $last) = /(.+?)(?:(\d+)?-(\d+))?$/;
+
+   unless (defined $last)
+   {
+      # trailing range
+      if ($host =~ /(\d+)?-$/)
+      {
+         unless (defined $1)
+         {
+            chop $host;
+            push @hosts, $host.1, $host.2;
+         } else {
+            warn RED.'dash forbidden at end'.RESET, "\n";
+         }
+      } else {
+         push @hosts, $host;
+      }
+
+      next;
+   }
+
+   $first //= 1;
+   $last  //= 1;
+
+   $last > $first or die RED.'non ascending range detected'.RESET, "\n";
+
+   push @hosts, map $host.$_, $first..$last;
+}
 
 my @nb_sessions = `tmux ls -F#S`;
 say grep (/^\d/, @nb_sessions);
