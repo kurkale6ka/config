fc0790ad68ce139d8e5218f01ff96ec3037c1be5
Dimitar Dimitrov <mitkofr@yahoo.fr>
2023-01-09 10:48:45 +0000

    Add -I since I now ignore ".git/" in ~/.gitignore

--- git.bash
+++ git.bash
@@ -52,7 +52,7 @@ git config --global alias.o browse
 git config --global alias.st status
 git config --global alias.cm commit
 git config --global alias.co checkout
-git config --global alias.bu '!fd --strip-cwd-prefix -FH -td .git | parallel --no-notice --tag --tagstring {//} git -C {//}'
+git config --global alias.bu '!fd --strip-cwd-prefix -FIH -td .git | parallel --no-notice --tag --tagstring {//} git -C {//}'
 git config --global alias.br '!git -P branch'
 git config --global alias.sha '!git -P show --oneline --quiet'
 git config --global alias.last '!git -P log -1 --stat'
