57e38046183d81cb8b7f147f9fb1a25554a85d72
Dimitar Dimitrov <mitkofr@yahoo.fr>
2023-03-04 18:30:50 +0000

    REPOS_BASE/github

--- git.bash
+++ git.bash
@@ -12,7 +12,7 @@ fi
 git config --global user.name 'Dimitar Dimitrov'
 git config --global init.defaultBranch main
 git config --global color.ui true
-git config --global core.excludesfile "$REPOS_BASE"/config/dotfiles/.gitignore
+git config --global core.excludesfile "$REPOS_BASE"/github/config/dotfiles/.gitignore
 
 # mitigate issues when pulling force-pushed commits
 git config --global pull.rebase true
